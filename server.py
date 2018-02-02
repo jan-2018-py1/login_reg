c2fadc600c3f38640ff40cd99a35162f837d1e97
minh <nguyenhminhp@gmail.com>
2018-02-01 16:20:05 -0800

    hot fix

---
+++ server.py
@@ -0,0 +1,19 @@
+from myconnection import MySQLConnector
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "My secret session key"
+mysql = MySQLConnector(app, "login_reg_jan_2018")
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+@app.route("/register", methods=["POST"])
+def register():
+	print request.form
+# First Name - letters only, at least 2 characters and that it was submitted
+# Last Name - letters only, at least 2 characters and that it was submitted
+# Email - Valid Email format, and that it was submitted
+# Password - at least 8 characters, and that it was submitted
+# Password Confirmation - matches password
+
+app.run(debug=True)
